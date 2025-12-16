eb2fe2b0cc66071a60293b3e023951e902656b16
Hassan Ghayyada <7assan.gghassan@gmail.com>
2025-12-16 16:30:30 +0300

    added 2 methods addCourse and getAverage

--- models.js
+++ models.js
@@ -10,4 +10,13 @@ export class Student {
     this.name = name;
     this.courses = courses;
   }
+  addCourse(courseId, grade) {
+    this.courses.push({ courseId, grade });
+  }
+
+  getAverage() {
+    if (this.courses.length === 0) return 0;
+    return this.courses.reduce((acc, c) => acc + c.grade, 0) / this.courses.length;
+  }
+
 }
