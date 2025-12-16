892f2b2710b11a2f53ab6663165b828ac9807da5
Hassan Ghayyada <7assan.gghassan@gmail.com>
2025-12-16 16:24:06 +0300

    make student id immutable using the Object.defineProperty method

--- models.js
+++ models.js
@@ -1,5 +1,12 @@
 export class Student {
   constructor(id, name, courses = []) {
+    Object.defineProperty(this, "id", {
+    value: id,
+    writable: false,
+    configurable: false,
+    enumerable: true
+   });
+
     this.name = name;
     this.courses = courses;
   }
