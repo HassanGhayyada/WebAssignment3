338cc067f7f4e55881f7a2e7c725e8099c361a52
Hassan Ghayyada <7assan.gghassan@gmail.com>
2025-12-16 17:08:43 +0300

    we find the top student using reduce and higher-order student filtering function

--- analytics.js
+++ analytics.js
@@ -9,3 +9,14 @@ export function calculateClassAverage(students, courseId) {
   const sum = grades.reduce((a, b) => a + b, 0);
   return sum / grades.length;
 }
+export function findTopStudent(students) {
+  if (students.length === 0) return null;
+
+  return students.reduce((best, current) =>
+    current.getAverage() > best.getAverage() ? current : best
+  );
+}
+
+export function filterStudents(students, criteriaFn) {
+  return students.filter(criteriaFn);
+}
